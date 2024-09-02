63c78739b20f21ffdd5e5c0ac6954d8da5f253bf
TallmanCode <steve@tallmancode.co.za>
2024-09-02 21:24:02 +0200

    start of swiper component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
 <script setup lang="ts">
 import usePhotos from "./composables/usePhotos.ts";
+import Swiper from "./components/Swiper.vue";
+import {ref} from "vue";
+
+const queue = ref<{src: string}[]>([])
+const photosList = ref<string[]>([])
+const isLoading = ref(true)
+const hasError = ref<string | false>(false);
+const offset = ref(0)
 
 const pexel = usePhotos()
-pexel?.getPhotos().then(res => console.log(res))
+if(pexel){
+    pexel
+        .getPhotos()
+        .then((photos) => {
+            photosList.value = photos
+            buildQueue();
+            isLoading.value = false
+        }).catch((error) => {
+            hasError.value = error
+        })
+}else{
+    hasError.value = 'Something went wrong.'
+    isLoading.value = false
+}
+
+const buildQueue = (count = 5, append = true) => {
+    const list = []
+    for (let i = 0; i < count; i++) {
+        list.push({src: photosList.value[offset.value]})
+        offset.value++
+    }
+    if (append) {
+        queue.value = queue.value.concat(list)
+    } else {
+        queue.value.unshift(...list)
+    }
+}
+
+
+
 </script>
 
 <template>
     <div>
-
+        <Swiper v-model:queue="queue"/>
     </div>
 </template>
 
---
+++ src/components/Swiper.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import {PropType} from "vue";
+
+const queue = defineModel('queue', {type: [Array<{src: string}>] as PropType<Array<{src: string}>> , default: () => []})
+
+</script>
+
+<template>
+<div class="swiper" ref="root">
+    <img v-for="item in queue" :src="item.src" alt="">
+</div>
+</template>
+
+<style scoped>
+</style>

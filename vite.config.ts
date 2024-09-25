c6b21a775c3bfb19deb23710023491109279723f
TallmanCode <steve@tallmancode.co.za>
2024-09-25 21:43:24 +0200

    build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
+import commonjs from "vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
